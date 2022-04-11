223a1d6710cd1bc11ccf3f6f939b31d10e6e710a
Kenneth Lai <kennethlai@Kenneths-MacBook-Air-2.local>
2022-04-11 12:22:50 -0700

    Initial enemy classes

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,29 @@
+//Base Enemy class
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour//perhaps abstract
+{
+
+    public int health;
+    public int damage;
+    public int xp;
+    public int gold;
+    
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    */
+}
