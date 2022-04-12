70a4c171b41ca77f92f2a3643ce3f6d1090f4d82
Danz Moses <danzmoses@gmail.com>
2022-04-12 16:04:46 -0700

    Added Player_SwordAttackDown animation Added WorldOfSolaria SpritePack Player_SwordAttackDown plays when player presses left click Added PlayerIdle animation

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Play an attack animation
+        animator.SetTrigger("Attack");
+        // Detect enemies in range
+        // Apply damage
+    }
+}
