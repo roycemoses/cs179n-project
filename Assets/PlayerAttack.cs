5c312f04534bce6267a251f43e4adb02931ba00d
Danz Moses <danzmoses@gmail.com>
2022-04-12 16:28:04 -0700

    Added AttackPoint as child of Player AttackPoint is the center of an attack

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
 
     // Update is called once per frame
     void Update()
@@ -19,7 +22,22 @@ public class PlayerAttack : MonoBehaviour
     {
         // Play an attack animation
         animator.SetTrigger("Attack");
+
         // Detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
         // Apply damage
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
     }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
 }
