4f832eb0465632444828f8034f0d2cc22c1524ca
Danz Moses <danzmoses@gmail.com>
2022-04-15 21:09:57 -0700

    Player ignores collision with "Enemies" layer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     {
         currHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        Physics2D.IgnoreLayerCollision(this.gameObject.layer, LayerMask.NameToLayer("Enemies"));
     }
 
     // Update is called once per frame
@@ -27,4 +28,9 @@ public class Player : MonoBehaviour
         currHealth -= damage;
         healthBar.SetHealth(currHealth);
     }
+
+    // private void OnCollisionEnter2D(Collision2D other) {
+    //     if (other.gameObject.layer == LayerMask.NameToLayer("Enemies"))
+    //         Physics2D.IgnoreCollision(GetComponent<Collider>(), collider);
+    // }
 }
