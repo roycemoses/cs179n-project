3985d5ef714f302af846f348689cfb43a587c7e5
roycemoses <royce.kmoses@gmail.com>
2022-04-21 13:59:11 -0700

    Fixed colliding with Player bug (hotfix, not using layers

--- Assets/Scripts/Projectile/ProjectileScript.cs
+++ Assets/Scripts/Projectile/ProjectileScript.cs
@@ -8,8 +8,12 @@ public class ProjectileScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        // Destroy(effect, 5f);
-        Destroy(gameObject);
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            // GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            // Destroy(effect, 5f);
+            Debug.Log(collision.collider);
+            Destroy(gameObject);
+        }
     }
 }
