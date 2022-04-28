e17065cd4552580d101458595a183214c2d148e8
crystlfng <crystlfng@gmail.com>
2022-04-28 01:52:32 -0700

    home base scene plus scene transition

---
+++ Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class VectorValue : ScriptableObject
+{
+    public Vector2 initialValue;
+}
