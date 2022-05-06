30e37dc462851a8acebe1142ee7763ab91ca3313
crystlfng <crystlfng@gmail.com>
2022-05-05 18:18:04 -0700

    scene transitions

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,13 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    public Animator transition;
     public string sceneToLoad;
+    public float transtionTime = 1;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
     public void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player") && !other.isTrigger){
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
         }
+    }
+
+    IEnumerator LoadLevel(int levelIndex){
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transtionTime);
+        SceneManager.LoadScene(levelIndex);
     }
 }
