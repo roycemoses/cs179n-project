0fd017425474ad07b3011c101c34c84e7bda433f
danzmoses <danzmoses@gmail.com>
2022-05-11 11:29:18 -0700

    Added ShopItem.cs ShopItem.cs will track whether a player is in range to buy an item It will also handle buying the item The script should be attached to the item's buy range hitbox

---
+++ Assets/ShopItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+
+    bool playerInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        playerInRange = true;
+        Debug.Log("Player is in range!");
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        playerInRange = false;
+        Debug.Log("Player left the buy range!");
+    }
+}
