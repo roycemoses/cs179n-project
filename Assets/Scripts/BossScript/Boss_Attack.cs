07659176dedbed620847f03a7b4fbe063f2de584
crystlfng <crystlfng@gmail.com>
2022-05-23 20:47:03 -0700

    charge works there is a buggy glide tho

--- Assets/Scripts/BossScript/Boss_Attack.cs
+++ Assets/Scripts/BossScript/Boss_Attack.cs
@@ -11,14 +11,17 @@ public class Boss_Attack : MonoBehaviour
 
 
     //parameter passed in is the object this Boss object is colliding with
-    void OnTriggerEnter(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         //check if the object you are colliding with has tag player
        if (col.CompareTag("Player"))
         {
+            
             Debug.Log("player and boss colliding");
             //have player take damage
             col.gameObject.GetComponent<Player>().TakeDamage(damage);
+            GetComponent<Boss_Run>().fast = false;
+            animator.SetBool("isMoving", false);
         }
     }
 }
--- Assets/Scripts/BossScript/Boss_Run.cs
+++ Assets/Scripts/BossScript/Boss_Run.cs
@@ -16,67 +16,81 @@ public class Boss_Run : MonoBehaviour
     private Transform target;//Player
 
     private Animator animator;
-    private double timer = 10;
+    private double timer = 5;
+
+    public bool fast = false;
+
     
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         initialPos = gameObject.transform.position;
-        speed2 = speed + 2;
+        speed2 = speed + 5;
         speedoriginal = speed;
         animator = GetComponent<Animator>();
     }
     
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        
+        if (fast)
         {               
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
-            Vector2 direction = transform.position - target.position;
-            Debug.Log("direction: " + direction);
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             animator.SetBool("isMoving", true);
-
-            if ( ((angle >= 135 && angle <= 180) || (angle <= -135 && angle >= -180)) ) // RIGHT
-            {
-                animator.SetFloat("Xinput", 1.0f);
-                animator.SetFloat("Yinput", 0f);
-                // Debug.Log("RIGHT");
-            }
-            else if ( ((angle <= 45 && angle >= 0) || (angle >= -45 && angle <= 0)) ) // LEFT
-            {
-                animator.SetFloat("Xinput", -1.0f);
-                animator.SetFloat("Yinput", 0f);
-                // Debug.Log("LEFT");
-            }
-            else if (angle <= 135 && angle >= 45) // DOWN
-            {
-                animator.SetFloat("Xinput", 0f);
-                animator.SetFloat("Yinput", -1.0f);
-                // Debug.Log("DOWN");               
-            }
-            else if (angle >= -135 && angle <= -45) // UP
-            {
-                animator.SetFloat("Xinput", 0f);
-                animator.SetFloat("Yinput", 1.0f);
-                // Debug.Log("UP");   
-            }
-            
-            if(Vector2.Distance(transform.position, target.position) < 10){  
-                speed = speed2;
-            }
-            
+            dir();
         }
         else{
-            while(timer > 0){
-                // Debug.Log("stop moving");
-                animator.SetBool("isMoving", false);
-                speed = 3;
-                timer -= 0.001*Time.deltaTime;
-            }
-            timer = 10;
+            animator.SetBool("isMoving", false);
+            dir();
         }
+    }
+
+    void dir(){
+            Debug.Log(timer);
+
+            
+            if(fast){
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+
+                Vector2 direction = transform.position - target.position;
+                Debug.Log("direction: " + direction);
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                if ( ((angle >= 135 && angle <= 180) || (angle <= -135 && angle >= -180)) ) // RIGHT
+                {
+                    animator.SetFloat("Xinput", 1.0f);
+                    animator.SetFloat("Yinput", 0f);
+                    // Debug.Log("RIGHT");
+                }
+                else if ( ((angle <= 45 && angle >= 0) || (angle >= -45 && angle <= 0)) ) // LEFT
+                {
+                    animator.SetFloat("Xinput", -1.0f);
+                    animator.SetFloat("Yinput", 0f);
+                    // Debug.Log("LEFT");
+                }
+                else if (angle <= 135 && angle >= 45) // DOWN
+                {
+                    animator.SetFloat("Xinput", 0f);
+                    animator.SetFloat("Yinput", -1.0f);
+                    // Debug.Log("DOWN");               
+                }
+                else if (angle >= -135 && angle <= -45) // UP
+                {
+                    animator.SetFloat("Xinput", 0f);
+                    animator.SetFloat("Yinput", 1.0f);
+                    // Debug.Log("UP");   
+                }
+
+                
+            if(timer <= 0){
+                timer =2;
+                fast = !fast;
+            }
+            else{
+                timer -= Time.deltaTime;
+            }
+            
+            
+            
     }
         
 }
