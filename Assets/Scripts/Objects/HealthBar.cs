03fb36151a77305c39298c4327ef24495edf8d9c
danzmoses <danzmoses@gmail.com>
2022-05-23 21:08:27 -0700

    Almost done Need to separate health bar script from player vs enemies

--- Assets/Scripts/Objects/HealthBar.cs
+++ Assets/Scripts/Objects/HealthBar.cs
@@ -12,9 +12,40 @@ public class HealthBar : MonoBehaviour
     public Image fill;
     public GameObject instance;
 
+    private void Awake() {
+        if (instance == null) {
+            instance = GameObject.Find("Canvas");
+            Debug.Log(instance);
+            DontDestroyOnLoad(instance);
+        }
+        else
+            Destroy(GameObject.Find("Canvas"));
+    }
+
+    private void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable() {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Canvas.ForceUpdateCanvases();
+    }
+
     private void Start() {
+        if (player == null) {
+            player = Resources.Load<Player>("Player");
+            if (player == null) { Debug.Log("Rip"); }
+        }
         SetMaxHealth(player.baseHealth);
         SetHealth(player.currHealth);
+    }
+
+    private void Update() {
+        SetHealth(player.currHealth);  
     }
 
     public void SetMaxHealth(int health)
