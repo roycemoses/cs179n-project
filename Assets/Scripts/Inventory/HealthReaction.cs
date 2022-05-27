89a2c6e593d27b09d2a538457378437d1b44bf21
alejandraavitia03 <aleavitia03@gmail.com>
2022-05-26 19:21:37 -0700

    Pick Up Works

--- Assets/Scripts/Inventory/HealthReaction.cs
+++ Assets/Scripts/Inventory/HealthReaction.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class HealthReaction : MonoBehaviour
 {
-    public Player player;
+    //[SerializeField] private PlayerInventory playerInventory;
+    //[SerializeField] private InventoryItem thisItem;
+    public Player hero;
+    public int potion_inc;
     
-    // public void Use(int amount)
-    // {
-    //     player.currHealth += amount;
-    // }
-}
+    // Use this for initialization
+    private void Start () {
+        print($"Before: {hero.equipHealth}");//debug
+    }
+ 
+    // Update is called once per frame
+    private void Update () {
+        //OnCollisionEnter(Player);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            hero.currHealth = hero.currHealth + potion_inc;
+            hero.healthBar.SetHealth(hero.currHealth);
+            //hero.dam_red = 2;
+            Destroy(this.gameObject);
+        }
+    }     
+ }
