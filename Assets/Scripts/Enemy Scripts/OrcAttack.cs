8b0813469dfc6b2609bcef7c593a53eb6272dbdd
roycemoses <royce.kmoses@gmail.com>
2022-06-01 06:46:06 -0700

    Implemented Orc, everything should work. Balancing may be needed

--- Assets/Scripts/Enemy Scripts/OrcAttack.cs
+++ Assets/Scripts/Enemy Scripts/OrcAttack.cs
@@ -9,28 +9,68 @@ public class OrcAttack : MonoBehaviour
 
     public float surprisedSeconds = 1f;
     public float forcedIdleSeconds = 0.5f;
+    public float waitTime = 0f;
+    public float startWaitTime = 2f;
+    public float chargeSeconds = 1f;
+    public float attackAnimationTime = 1f;
     public int damage = 15;
-    public float followRange = 10f;
+    public float detectRange = 10f;
+    public float attackTriggerRange = 1f;
     public bool isAttacking;
+    public bool inRange;
+    public float speed;
+
+    public List<Transform> moveSpots;
+    private int randomSpot;
 
     private Animator animator;
 
     public AudioSource meleeAttackSound;
 
-    Vector3 playerPos;
+    public GameObject player;
 
+    private Vector3 playerPos;
+
+    public GameObject hitboxPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        playerPos = GameObject.Find("Player").transform.position;
+        player = GameObject.Find("Player");
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Count);
+        if (player == null)
+        {
+            Debug.Log("Error: Enemy cannot find player");
+        }
+
+        if (moveSpots.Count == 0)
+        {
+            GameObject topLeft = new GameObject();
+            topLeft.transform.position = transform.position + new Vector3(-2, 2, 0);
+            GameObject topRight = new GameObject();
+            topRight.transform.position = transform.position + new Vector3(2, 2, 0);
+            GameObject bottomLeft = new GameObject();
+            bottomLeft.transform.position = transform.position + new Vector3(-2, -2, 0);
+            GameObject bottomRight = new GameObject();
+            bottomRight.transform.position = transform.position + new Vector3(2, -2, 0);
+            GameObject center = new GameObject();
+            center.transform.position = transform.position;            
+            moveSpots.Add(topLeft.transform);
+            moveSpots.Add(topRight.transform);
+            moveSpots.Add(bottomLeft.transform);
+            moveSpots.Add(bottomRight.transform);
+            moveSpots.Add(center.transform);
+        }
+        animator.SetBool("Patrol Idle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
         // check if player is in enemy's attack range
+        playerPos = player.transform.position;
         float distance = Vector2.Distance(playerPos, transform.position);
         if (distance > GetComponent<Enemy>().attackRange)
         {
@@ -40,19 +80,35 @@ public class OrcAttack : MonoBehaviour
         {
             inRange = true;
             if (animator.GetBool("Patrol Idle") || animator.GetBool("Patrol Moving"))
+            {
                 animator.SetBool("Surprised Idle", true);
+                animator.SetBool("Patrol Idle", false);
+                animator.SetBool("Patrol Moving", false);
+            }
         }
 
         if (animator.GetBool("Patrol Idle"))
         {
-
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Count);
+                animator.SetBool("Patrol Moving", true);
+                animator.SetBool("Patrol Idle", false);
+            }
+            if (Vector2.Distance(playerPos, transform.position) <= detectRange) // If the Player gets too close to Enemy!
+            {
+                // Go to Surprised state and reset the wait time!
+                animator.SetBool("Surprised Idle", true);
+                animator.SetBool("Patrol Idle", false);
+            }
         }
         else if (animator.GetBool("Patrol Moving"))
         {
-            // patrol direction
-            Vector2 patrolDirection = transform.position - moveSpots[randomSpot].position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            if (patrolDirection != Vector2.zero)
+            // look direction
+            Vector2 lookDirection = transform.position - moveSpots[randomSpot].position;
+            float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+            if (lookDirection != Vector2.zero)
             {
                 if ( ((angle >= 135 && angle <= 180) || (angle <= -135 && angle >= -180)) ) // RIGHT
                 {
@@ -77,122 +133,172 @@ public class OrcAttack : MonoBehaviour
             }
 
             transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-
             
-            
+            if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) <= 0.2f)
+            {
+                waitTime = startWaitTime;
+                animator.SetBool("Patrol Idle", true);
+                animator.SetBool("Patrol Moving", false);
+            }
+
+            if (Vector2.Distance(playerPos, transform.position) <= detectRange) // If the Player gets too close to Enemy!
+            {
+                // Go to Surprised state and reset the wait time!
+                animator.SetBool("Surprised Idle", true);
+                animator.SetBool("Patrol Moving", false);
+            }
         }
-        else if (animator.GetBool("Forced Idle"))
+        else if (animator.GetBool("Surprised Idle"))
         {
-            // orc to player direction
-            Vector2 direction = transform.position - playerPos;
+            waitTime = startWaitTime;
 
-        }
-        
+            Vector2 lookDirection = transform.position - playerPos;
+            float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 
-
-
-
-
-
-
-
-
-
-
-
-        if (Time.time >= nextAttackTime && inRange && !isAttacking)
-        {
-            Debug.Log("Attacking...");   
-            StartCoroutine(Attack());   
-        }
-    }
-
-
-    private IEnumerator Attack()
-    {
-        isAttacking = true;
-        if (isSleeping)
-        {
-            Debug.Log("Surprised");
-            isSleeping = false;
-            animator.SetBool("isSleeping", false);
-            yield return new WaitForSeconds(surprisedSeconds);
-        }
-
-        if (!inRange)
-        {
-            isPreparing = false;
-            animator.SetBool("isPreparing", false);  
-        }
-        else
-        {   
-            isPreparing = true;
-            animator.SetBool("isPreparing", true);  
-
-            yield return new WaitForSeconds(chargeSeconds);
-
-            Debug.Log("Attacking");
-            isMoving = true;
-            animator.SetBool("isMoving", true);
-            isPreparing = false;
-            animator.SetBool("isPreparing", false);
-
-            Vector3 playerPos = GameObject.Find("Player").transform.position;
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            rb.drag = 0f;
-            rb.AddForce((playerPos - transform.position).normalized * speed, ForceMode2D.Force);
-        
-
-            Vector2 direction = transform.position - playerPos;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;  
             if ( ((angle >= 135 && angle <= 180) || (angle <= -135 && angle >= -180)) ) // RIGHT
             {
                 animator.SetFloat("Xinput", 1.0f);
                 animator.SetFloat("Yinput", 0f);
-                // Debug.Log("RIGHT");
             }
             else if ( ((angle <= 45 && angle >= 0) || (angle >= -45 && angle <= 0)) ) // LEFT
             {
                 animator.SetFloat("Xinput", -1.0f);
                 animator.SetFloat("Yinput", 0f);
-                // Debug.Log("LEFT");
             }
             else if (angle <= 135 && angle >= 45) // DOWN
             {
                 animator.SetFloat("Xinput", 0f);
-                animator.SetFloat("Yinput", -1.0f);
-                // Debug.Log("DOWN");               
+                animator.SetFloat("Yinput", -1.0f);           
             }
             else if (angle >= -135 && angle <= -45) // UP
             {
                 animator.SetFloat("Xinput", 0f);
-                animator.SetFloat("Yinput", 1.0f);
-                // Debug.Log("UP");   
-            }        
+                animator.SetFloat("Yinput", 1.0f);  
+            }
 
-            yield return new WaitForSeconds(movingSeconds);
-
-            rb.drag = 3f;
-
-            isMoving = false;
-            animator.SetBool("isMoving", false);
+            if (surprisedSeconds <= 0)
+            {
+                animator.SetBool("Following", true);
+                animator.SetBool("Surprised Idle", false);
+            }
+            else
+            {
+                surprisedSeconds -= Time.deltaTime;
+            }
         }
-        
+        else if (animator.GetBool("Following"))
+        {            
+            surprisedSeconds = 0;
 
-        isInIdleState = true;
-        animator.SetBool("isInIdleState", true);
+            Vector2 moveDirection = transform.position - playerPos;
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+
+            if ( ((angle >= 135 && angle <= 180) || (angle <= -135 && angle >= -180)) ) // RIGHT
+            {
+                animator.SetFloat("Xinput", 1.0f);
+                animator.SetFloat("Yinput", 0f);
+            }
+            else if ( ((angle <= 45 && angle >= 0) || (angle >= -45 && angle <= 0)) ) // LEFT
+            {
+                animator.SetFloat("Xinput", -1.0f);
+                animator.SetFloat("Yinput", 0f);
+            }
+            else if (angle <= 135 && angle >= 45) // DOWN
+            {
+                animator.SetFloat("Xinput", 0f);
+                animator.SetFloat("Yinput", -1.0f);           
+            }
+            else if (angle >= -135 && angle <= -45) // UP
+            {
+                animator.SetFloat("Xinput", 0f);
+                animator.SetFloat("Yinput", 1.0f);  
+            }
+
+            if (Vector2.Distance(playerPos, transform.position) >= detectRange) // The player got too far away! Follow range exceeded; back to patrol
+            {
+                animator.SetBool("Following", false);
+                animator.SetBool("Patrol Moving", true);
+            }
+            
+            if (Vector2.Distance(playerPos, transform.position) > attackTriggerRange) // if the distance between the orc and player is too far to attack, orc follows!
+                transform.position = Vector2.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+
+            if (Time.time >= nextAttackTime && Vector2.Distance(playerPos, transform.position) <= attackTriggerRange && !isAttacking) // The Orc has no cooldown, Orc is close enough, and is not attacking: Start the Attack Coroutine!
+            {
+                Debug.Log("Attacking...");
+                animator.SetBool("Following", false);
+                animator.SetBool("Preparing", true);
+                StartCoroutine(Attack());
+            }
+        }
+        else if (animator.GetBool("Forced Idle"))
+        {
+            // orc to player direction
+            // Vector2 direction = transform.position - playerPos;
+
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        isAttacking = true;
+        // TODO: Spawn outline of real hitbox!
+
+        Vector3 lookDirection = transform.position - playerPos; // Look at the player while charging
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;  
+        if ( ((angle >= 135 && angle <= 180) || (angle <= -135 && angle >= -180)) ) // RIGHT
+        {
+            animator.SetFloat("Xinput", 1.0f);
+            animator.SetFloat("Yinput", 0f);
+        }
+        else if ( ((angle <= 45 && angle >= 0) || (angle >= -45 && angle <= 0)) ) // LEFT
+        {
+            animator.SetFloat("Xinput", -1.0f);
+            animator.SetFloat("Yinput", 0f);
+        }
+        else if (angle <= 135 && angle >= 45) // DOWN
+        {
+            animator.SetFloat("Xinput", 0f);
+            animator.SetFloat("Yinput", -1.0f);             
+        }
+        else if (angle >= -135 && angle <= -45) // UP
+        {
+            animator.SetFloat("Xinput", 0f);
+            animator.SetFloat("Yinput", 1.0f);
+        }
+
+
+        GameObject hitbox = Instantiate(hitboxPrefab, transform.position - lookDirection * 1.25f, Quaternion.Euler(0, 0, angle+90f));
+        hitbox.transform.parent = gameObject.transform;
+        hitbox.GetComponent<BoxCollider2D>().enabled = false;
+        hitbox.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,.55f); // change opacity to 55%
+
+        yield return new WaitForSeconds(chargeSeconds);
+
+        animator.SetBool("Preparing", false);
+        animator.SetBool("Attacking", true);
+        
+        // TODO: Hitbox ACTIVATES (enable collider) and deals damage to the player.
+        hitbox.GetComponent<BoxCollider2D>().enabled = true;
+        hitbox.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f); // change opacity to 0%
+
+        yield return new WaitForSeconds(attackAnimationTime);
+
+        animator.SetBool("Forced Idle", true);
+        animator.SetBool("Attacking", false);
+
+        Destroy(hitbox);
+
+        yield return new WaitForSeconds(forcedIdleSeconds);
+
+        if (inRange)
+            animator.SetBool("Following", true);
+        else
+            animator.SetBool("Patrol Moving", true);
+        
+        animator.SetBool("Forced Idle", false);
 
         nextAttackTime = Time.time + 1f / GetComponent<Enemy>().attackRate;
         isAttacking = false;
-    }
-
-
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
-        {
-            Debug.Log("took dmg");
-            col.gameObject.GetComponent<PlayerManager>().TakeDamage(damage);
-        }
     }
 }
